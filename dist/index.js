206f5d425371bcd157637f3cfa36da078f2b9c75
Gunjan Modi <gunjan@nividata.com>
2020-02-10 17:27:58 +0530

    Adding dist

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview added by tsickle
+ * Generated from: index.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+export { FormioModule, Formio, FormioUtils, Templates, Components, FormioAppConfig, FormioError, FormioService, extendRouter, FormioBaseComponent, FormioComponent, FormBuilderComponent, FormioLoader, FormioLoaderComponent, FormioAlerts, FormioAlertsComponent } from './core';
+export { createCustomFormioComponent } from './custom-component/create-custom-component';
+export { registerCustomTag, registerCustomTags, registerCustomFormioComponent, registerCustomFormioComponentWithClass } from './custom-component/register-custom-component';
