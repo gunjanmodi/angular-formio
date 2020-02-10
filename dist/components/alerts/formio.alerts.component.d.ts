611d3ae8ee00127cf5a9c94f1e8d4e44fddce17b
Gunjan Modi <gunjan@nividata.com>
2020-02-10 18:10:55 +0530

    removing dist

--- dist/components/alerts/formio.alerts.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnInit } from '@angular/core';
-import { FormioAlerts } from './formio.alerts';
-export declare class FormioAlertsComponent implements OnInit {
-    alerts: FormioAlerts;
-    ngOnInit(): void;
-}
--- dist/grid/form/FormGridBody.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { Formio } from 'formiojs';
-import { GridBodyComponent } from '../GridBodyComponent';
-export declare class FormGridBodyComponent extends GridBodyComponent {
-    load(formio: Formio, query?: any): any;
-}
